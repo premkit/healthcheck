1f6441c3ffc0a404d43dbf5b2efbf839c8189e8e
Marc Campbell <marc.e.campbell@gmail.com>
2016-06-30 19:36:23 -0700

    Add a few more http tests

--- httpcheck/checks_test.go
+++ httpcheck/checks_test.go
@@ -57,3 +57,48 @@ func TestHTTPHealthcheckRunGetSuccessOn404(t *testing.T) {
 
 	assert.Equal(t, healthcheck.HealthcheckResponseAvailable, resp.Status())
 }
+
+// Run a http healthcheck that returns a degraded response
+func TestHTTPHealthcheckDegraded(t *testing.T) {
+	check := HTTPHealthcheck{
+		Endpoint: "https://github.com/premkit/invalid",
+		Method:   "GET",
+		ResponseAvailable: []int{
+			http.StatusOK,
+		},
+		ResponseDegraded: []int{
+			http.StatusNotFound,
+		},
+		ResponseUnavailable: []int{
+			http.StatusCreated,
+		},
+	}
+
+	resp, err := check.RunSynchronously()
+	require.NoError(t, err)
+
+	assert.Equal(t, healthcheck.HealthcheckResponseDegraded, resp.Status())
+}
+
+// Run a http healthcheck that returns an unavailable response
+func TestHTTPHealthcheckUnavailable(t *testing.T) {
+	check := HTTPHealthcheck{
+		Endpoint: "https://github.com/premkit/invalid",
+		Method:   "GET",
+		ResponseAvailable: []int{
+			http.StatusOK,
+		},
+		ResponseDegraded: []int{
+			http.StatusCreated,
+		},
+		ResponseUnavailable: []int{
+			http.StatusNotFound,
+			http.StatusForbidden,
+		},
+	}
+
+	resp, err := check.RunSynchronously()
+	require.NoError(t, err)
+
+	assert.Equal(t, healthcheck.HealthcheckResponseUnavailable, resp.Status())
+}
